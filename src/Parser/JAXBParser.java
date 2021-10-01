dc50eb7b0a0bd80e0c1eb4cb9c4e223f20d6b201
RoyerZV <roy.er2912@hotmail.com>
2021-09-30 22:28:39 -0600

    - Added JAXBParser class. - Added points 2.1 and 2.3 for Prestamos in modeloPrestamos. Pending point 2.3.

---
+++ src/Parser/JAXBParser.java
@@ -0,0 +1,33 @@
+package Parser;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import java.io.FileOutputStream;
+import java.io.File;
+
+public class JAXBParser {
+    public void marshall(Object obj, String filename){
+        try{
+            JAXBContext jContext = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshallObj = jContext.createMarshaller();
+            marshallObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshallObj.marshal(obj, new FileOutputStream(filename));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Object unmarshall(Object obj, String filename){
+        Object obj1 = null;
+        try{
+            File file = new File(filename);
+            JAXBContext jContext = JAXBContext.newInstance(obj.getClass());
+            Unmarshaller unmarshallerObj = jContext.createUnmarshaller();
+            obj1 = (Object) unmarshallerObj.unmarshal(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return obj1;
+    }
+}
--- src/Prestamos/modeloPrestamos.java
+++ src/Prestamos/modeloPrestamos.java
@@ -1,4 +1,41 @@
 package Prestamos;
 
 public class modeloPrestamos {
+    private double monto;
+    private double tasaDeInteres;
+    private double plazo;
+
+    public modeloPrestamos(double monto, double tasaDeInteres, double plazo) {
+        this.monto = monto;
+        this.tasaDeInteres = tasaDeInteres;
+        this.plazo = plazo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public double getTasaDeInteres() {
+        return tasaDeInteres;
+    }
+
+    public void setTasaDeInteres(double tasaDeInteres) {
+        this.tasaDeInteres = tasaDeInteres;
+    }
+
+    public double getPlazo() {
+        return plazo;
+    }
+
+    public void setPlazo(double plazo) {
+        this.plazo = plazo;
+    }
+
+    public double calculoCuota() {
+        return (monto * tasaDeInteres) / (1 - Math.pow((1 + tasaDeInteres), -plazo));
+    }
 }
