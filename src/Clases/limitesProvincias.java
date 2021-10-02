413dde283b8a7f5637af937086c57182c659b6f1
angelicarobles <maria.robles.azofeifa@est.una.ac.cr>
2021-10-01 23:15:15 -0600

    Vista Cliente added - Mapas puntos added

---
+++ src/Clases/limitesProvincias.java
@@ -0,0 +1,133 @@
+package Clases;
+
+import java.awt.*;
+
+public class limitesProvincias {
+
+    Polygon herediaProv, sanJoseProv, alajuelaProv, cartagoProv, guanacasteProv, limonProv, puntareanasProv;
+
+
+    public void limitCoordMapas(){
+
+        //PUNTOS HEREDIA
+        herediaProv.addPoint(263, 73);
+        herediaProv.addPoint(235,65);
+        herediaProv.addPoint(235,105);
+        herediaProv.addPoint(235,137);
+        herediaProv.addPoint(285,69);
+        herediaProv.addPoint(259,135);
+        herediaProv.addPoint(270,116);
+        herediaProv.addPoint(232,165);
+        herediaProv.addPoint(243,165);
+        herediaProv.addPoint(250,158);
+        herediaProv.addPoint(243,142);
+        herediaProv.addPoint(274,94);
+        herediaProv.addPoint(273,81);
+
+        //PUNTOS SAN JOSE
+
+        sanJoseProv.addPoint(260,140);
+        sanJoseProv.addPoint(248, 145);
+        sanJoseProv.addPoint(255, 159);
+        sanJoseProv.addPoint(261,152);
+        sanJoseProv.addPoint(268,165);
+        sanJoseProv.addPoint(251,166);
+        sanJoseProv.addPoint(249, 175);
+        sanJoseProv.addPoint(230,170);
+        sanJoseProv.addPoint(246,184);
+        sanJoseProv.addPoint(207,175);
+        sanJoseProv.addPoint(242,195);
+        sanJoseProv.addPoint(270,209);
+        sanJoseProv.addPoint(299,222);
+        sanJoseProv.addPoint(322,245);
+        sanJoseProv.addPoint(314,266);
+        sanJoseProv.addPoint(311,279);
+        sanJoseProv.addPoint(294,201);
+        sanJoseProv.addPoint(278,258);
+        sanJoseProv.addPoint(267,238);
+        sanJoseProv.addPoint(247,219);
+        sanJoseProv.addPoint(255,209);
+        sanJoseProv.addPoint(200,208);
+        sanJoseProv.addPoint(188,214);
+        sanJoseProv.addPoint(182,196);
+        sanJoseProv.addPoint(190,179);
+
+
+        //PUNTOS LIMON
+
+        limonProv.addPoint(298,52);
+        limonProv.addPoint(320,102);
+        limonProv.addPoint(350,142);
+        limonProv.addPoint(395,195);
+        limonProv.addPoint(428,233);
+        limonProv.addPoint(395,212);
+        limonProv.addPoint(386,230);
+        limonProv.addPoint(387,277);
+        limonProv.addPoint(361,249);
+        limonProv.addPoint(322,233);
+        limonProv.addPoint(341,185);
+        limonProv.addPoint(343,164);
+        limonProv.addPoint(305,162);
+        limonProv.addPoint(265,142);
+        limonProv.addPoint(280,100);
+        limonProv.addPoint(294,76);
+
+
+        //PUNTOS ALAJUELA
+
+        alajuelaProv.addPoint(230,65);
+        alajuelaProv.addPoint(224,164);
+        alajuelaProv.addPoint(191,171);
+        alajuelaProv.addPoint(172,176);
+        alajuelaProv.addPoint(186,161);
+        alajuelaProv.addPoint(171,126);
+        alajuelaProv.addPoint(156,98);
+        alajuelaProv.addPoint(106,60);
+        alajuelaProv.addPoint(75,46);
+        alajuelaProv.addPoint(97,28);
+        alajuelaProv.addPoint(137,44);
+        alajuelaProv.addPoint(167,26);
+        alajuelaProv.addPoint(204,40);
+
+        //PUNTOS GUANACASTE
+
+        guanacasteProv.addPoint(51,9);
+        guanacasteProv.addPoint(92,25);
+        guanacasteProv.addPoint(67,44);
+        guanacasteProv.addPoint(88,66);
+        guanacasteProv.addPoint(122,64);
+        guanacasteProv.addPoint(153,106);
+        guanacasteProv.addPoint(139,124);
+        guanacasteProv.addPoint(90,121);
+        guanacasteProv.addPoint(75,120);
+        guanacasteProv.addPoint(84,169);
+        guanacasteProv.addPoint(96,194);
+        guanacasteProv.addPoint(73,177);
+        guanacasteProv.addPoint(44,171);
+        guanacasteProv.addPoint(20,117);
+        guanacasteProv.addPoint(47,83);
+        guanacasteProv.addPoint(42,57);
+        guanacasteProv.addPoint(18,46);
+        guanacasteProv.addPoint(43,28);
+
+        //PUNTOS PUNTARENAS
+
+        puntareanasProv.addPoint(111,216);
+        puntareanasProv.addPoint(101,199);
+        puntareanasProv.addPoint(105,181);
+        puntareanasProv.addPoint(90,171);
+        puntareanasProv.addPoint(107,165);
+        puntareanasProv.addPoint(123,173);
+        puntareanasProv.addPoint(133,174);
+        puntareanasProv.addPoint(123,195);
+        puntareanasProv.addPoint(133,185);
+        puntareanasProv.addPoint(145,138);
+        puntareanasProv.addPoint(149,122);
+        puntareanasProv.addPoint(159,131);
+        puntareanasProv.addPoint(166,128);
+        puntareanasProv.addPoint(166,146);
+        puntareanasProv.addPoint(178,163);
+        puntareanasProv.addPoint(166,174);
+        puntareanasProv.addPoint(178,184);
+    }
+}
