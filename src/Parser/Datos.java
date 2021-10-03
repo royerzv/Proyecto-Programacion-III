b898106c7fbe4b30c0b31c9cd63f5c36ce140a0f
angelicarobles <maria.robles.azofeifa@est.una.ac.cr>
2021-10-03 16:21:15 -0600

    Clase Datos

--- src/Parser/Datos.java
+++ src/Parser/Datos.java
@@ -14,6 +14,44 @@ public class Datos {
     private ArrayList<Canton> cantones;
     private ArrayList<Distrito> distritos;
 
+    public Datos() {
+        clientes =  new ArrayList<>();
+        provincias = new ArrayList<>();
+        cantones = new ArrayList<>();
+        distritos = new ArrayList<>();
+    }
+
+    public ArrayList<Cliente> getClientes(){
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public ArrayList<Provincia> getProvincias() {
+        return provincias;
+    }
+
+    public void setProvincias(ArrayList<Provincia> provincias) {
+        this.provincias = provincias;
+    }
+
+    public ArrayList<Canton> getCantones() {
+        return cantones;
+    }
+
+    public void setCantones(ArrayList<Canton> cantones) {
+        this.cantones = cantones;
+    }
+
+    public ArrayList<Distrito> getDistritos() {
+        return distritos;
+    }
+
+    public void setDistritos(ArrayList<Distrito> distritos) {
+        this.distritos = distritos;
+    }
 }
 
 
